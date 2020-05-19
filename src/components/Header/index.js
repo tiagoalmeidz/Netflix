53d7d3647c6dba603a115a01cb8c6745c6eba073
Tiago Almeida <tiago.fernando.almeida@gmail.com>
2020-05-19 00:01:15 -0300

    Header e Buttons styles

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,16 +1,26 @@
 import React from "react";
 
 import logo from "../../utils/svg/logo.svg";
+import { MdKeyboardArrowRight } from "react-icons/md";
 import { Link } from "react-router-dom";
-import { HeaderComponent, Logo, HeaderTop } from "./styles";
+import { HeaderComponent, Logo, Title, SubTitle } from "./styles";
 
 export default function Header() {
   return (
     <HeaderComponent className="header-container">
-      <HeaderTop className="header-top">
+      <div className="header-top">
         <Logo src={logo} alt="Netflix Logo" />
         <Link className="signIn-btn">Sign In</Link>
-      </HeaderTop>
+      </div>
+      {/* Header Content */}
+      <div className="header-content">
+        <Title>See what's next.</Title>
+        <SubTitle>WATCH ANYWHERE. CANCEL ANYTIME.</SubTitle>
+        <Link className="main-offer-btn">
+          try it now
+          <MdKeyboardArrowRight className="icon-arrow-right" size={37} />
+        </Link>
+      </div>
     </HeaderComponent>
   );
 }
--- src/components/Header/styles.js
+++ src/components/Header/styles.js
@@ -28,9 +28,63 @@ export const HeaderComponent = styled.div`
       background: var(--main-red-hover);
     }
   }
+
+  .header-top {
+    position: relative;
+    height: 10rem;
+    z-index: 1;
+  }
+
+  .header-content {
+    width: 65%;
+    position: relative;
+    margin: 4.5rem auto 0;
+    display: flex;
+    justify-content: center;
+    align-content: center;
+    text-align: center;
+    flex-direction: column;
+    z-index: 1;
+  }
+
+  .main-offer-btn {
+    display: inline-block;
+    background: var(--main-red);
+    text-transform: uppercase;
+    border: none;
+    outline: none;
+    margin: 0 33%;
+    padding: 1.5rem;
+    border-radius: 0.1875rem;
+    font-size: 2rem;
+    text-align: center;
+    box-shadow: 0 1px 0 rgba(0, 0, 0, 0.45);
+    transition: background 0.3s ease-in;
+    cursor: pointer;
+
+    &:hover {
+      background: var(--main-red-hover);
+    }
+  }
+
+  .icon-arrow-right {
+    vertical-align: bottom;
+    margin-left: 1.5rem;
+  }
 `;
 
-export const HeaderTop = styled.div`
-  position: relative;
-  height: 10rem;
+/* Main Title */
+export const Title = styled.h1`
+  margin: 0 0 1.2rem;
+  font-size: 5rem;
+  font-weight: 700;
+  line-height: 6.5rem;
+`;
+
+export const SubTitle = styled.h2`
+  font-weight: 400;
+  font-size: 1.875rem;
+  line-height: 2rem;
+  margin: 0 0 1.875rem;
+  text-transform: uppercase;
 `;
